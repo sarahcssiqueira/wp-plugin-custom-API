b5b93460a447e41c06069898b63aed2393241d81
sarahcssiqueira <sarahcso@hotmail.com>
2023-06-07 16:51:22 -0300

    refactor: phpcbf

--- custom-endpoint.php
+++ custom-endpoint.php
@@ -8,26 +8,27 @@
 * Requires: 4.7
 *
 */
-add_action ( 'rest_api_init', 'custom_endpoint');
+add_action('rest_api_init', 'custom_endpoint');
 
-function custom_endpoint () {
-    register_rest_route (
+function custom_endpoint()
+{
+    register_rest_route(
         'mynamespace/v1',
         'myroute',
         array (
             'methods' => 'GET',
             'callback' => 'custom_endpoint_callback'
         )
-        );
+    );
+}
 
- }
-
-function custom_endpoint_callback( ) {
-  $data = array();
+function custom_endpoint_callback( )
+{
+    $data = array();
      
     $data['content'] = 'Hi! Customize it for the specific needs of your project';
       
-  return $data;
+    return $data;
 }
 
 ?>
